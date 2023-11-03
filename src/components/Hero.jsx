d0c013d47676ca695b858fdac921adfaa719bc7a
Tyson <tysonpederson95@gmail.com>
2023-11-03 18:50:41 -0400

    Add hero component file

---
+++ src/components/Hero.jsx
@@ -0,0 +1,13 @@
+export default function Hero() {
+  return (
+    <div id="hero" className="hero">
+      <div className="hero-content text-center">
+        <div className=" max-w-3xl">
+          <h1 className="text-5xl font-bold">Welcome to RuneStock!</h1>
+          <p className="text-2xl pt-6 pb-4">The preferred source for live item prices in Old School Runescape.</p>
+          <p className="max-w-2xl">Use the search bar above to search for items, or click on an item in the item history panel to jump back to a previously searched item.</p>
+        </div>
+      </div>
+    </div>
+  );
+}
